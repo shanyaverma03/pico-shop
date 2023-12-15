53776c455d7f31b7497fe46b103b7098f4d399ea
Shanya <shanyaverma03@gmail.com>
2023-12-15 14:45:07 +0530

    Add css for header

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 
+import classes from "./Header.module.css";
+
 const Header = () => {
   return (
-    <>
+    <div className={classes.wrapper}>
       <Link>PocoShop</Link>
-      <input type="text" placeholder="Search here" />
-      <Link to="login">Login</Link>
-      <Link>Cart</Link>
-    </>
+      <div className={classes.options}>
+        <input type="text" placeholder="Search here" />
+        <Link to="login">Login</Link>
+        <Link>Cart</Link>
+      </div>
+    </div>
   );
 };
 
