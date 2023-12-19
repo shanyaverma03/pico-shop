4037cf84667852123598264be1fc05ba8d885856
Shanya <shanyaverma03@gmail.com>
2023-12-20 02:12:32 +0530

    Add cart slice

---
+++ src/store/cartSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { cart: [] };
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      //if item is already there, increase the quantity, else push
+      const addedProduct = state.cart.find(
+        (product) => product.id === action.payload.id
+      );
+      if (addedProduct) {
+        state.cart.map((item) => {
+          if (item.id === action.payload.id) {
+            return { ...item, quantity: item.quantity + 1 };
+          }
+          return item;
+        });
+      } else {
+        state.cart.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeFromCart(state, action) {
+      state.cart = state.cart.filter((item) => item.id !== action.payload.id);
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authenticationSlice } from "./authenticationSlice";
 import { wishlistSlice } from "./wishlistSlice";
+import { cartSlice } from "./cartSlice";
 
 const store = configureStore({
   reducer: {
     authentication: authenticationSlice.reducer,
     wishlist: wishlistSlice.reducer,
+    cart: cartSlice.reducer,
   },
 });
 
