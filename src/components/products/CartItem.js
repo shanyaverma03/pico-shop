d1f4539b374d565c04dff45ee2bc358b718789c5
Shanya <shanyaverma03@gmail.com>
2023-12-20 02:26:29 +0530

    Dispatch actions for increasing and decreasing quantity

--- src/components/products/CartItem.js
+++ src/components/products/CartItem.js
@@ -1,22 +1,35 @@
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 import classes from "./CartItem.module.css";
 import { cartActions } from "../../store/cartSlice";
 
 const CartItem = ({ id, name, price, quantity }) => {
   const dispatch = useDispatch();
+  const [itemQuantity, setItemQuantity] = useState(quantity);
 
+  const decreaseQuantityHandler = () => {
+    setItemQuantity((prevQuantity) => prevQuantity - 1);
+    dispatch(cartActions.decreaseQuantity(id));
+  };
+
+  const increaseQuantityHandler = () => {
+    setItemQuantity((prevQuantity) => prevQuantity + 1);
+    dispatch(cartActions.increaseQuantity(id));
+  };
+
+  const totalPrice = itemQuantity * price;
   return (
     <div className={classes.wrapper}>
       <div className={classes.card}>
         <h3 className={classes.name}>{name}</h3>
         <p className={classes.price}>${price} per item</p>
         <div className={classes.quantity_control}>
-          <button>-</button>
-          <span className={classes.quantity}>{quantity}</span>
-          <button>+</button>
+          <button onClick={decreaseQuantityHandler}>-</button>
+          <span className={classes.quantity}>{itemQuantity}</span>
+          <button onClick={increaseQuantityHandler}>+</button>
         </div>
-        <p className={classes.total_price}>Total:88</p>
+        <p className={classes.total_price}>Total: {totalPrice}</p>
         <button
           className={classes.remove_button}
           onClick={() => dispatch(cartActions.removeFromCart(id))}
