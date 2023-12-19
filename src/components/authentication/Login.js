78cebb7cf102582867c03cd3982ec3558dcc5856
Shanya <shanyaverma03@gmail.com>
2023-12-20 00:24:56 +0530

    Handle login and logout

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,9 +1,22 @@
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 import classes from "./Auth.module.css";
+import { authenticationActions } from "../../store/authenticationSlice";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    dispatch(authenticationActions.login());
+    navigate("/");
+  };
+
   return (
     <div className={classes.wrapper}>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.container}>
           <label>
             <b>Username</b>
--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -1,16 +1,29 @@
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 import classes from "./Header.module.css";
+import { authenticationActions } from "../../store/authenticationSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(
+    (state) => state.authentication.isAuthenticated
+  );
+
   return (
     <div className={classes.wrapper}>
       <Link>PocoShop</Link>
       <div className={classes.options}>
         <input type="text" placeholder="Search here" />
-        <Link to="login">Login</Link>
-        <Link to="cart">Cart</Link>
-        <Link to="wishlist">Wishlist</Link>
+        {isAuthenticated ? (
+          <Link to="/" onClick={() => dispatch(authenticationActions.logout())}>
+            Logout
+          </Link>
+        ) : (
+          <Link to="login">Login</Link>
+        )}
+        {isAuthenticated && <Link to="cart">Cart</Link>}
+        {isAuthenticated && <Link to="wishlist">Wishlist</Link>}
       </div>
     </div>
   );
