edc6a285b9275a5a5788c698a5d34a47646c29e2
Shanya <shanyaverma03@gmail.com>
2023-12-20 00:31:28 +0530

    Handle add to cart and fav buttons

--- src/components/products/ProductItem.js
+++ src/components/products/ProductItem.js
@@ -1,6 +1,30 @@
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 import classes from "./ProductItem.module.css";
 
 const ProductItem = ({ name, description, price, category, isFavourite }) => {
+  const navigate = useNavigate();
+  const isAuthenticated = useSelector(
+    (state) => state.authentication.isAuthenticated
+  );
+
+  const addToCartHandler = () => {
+    if (isAuthenticated) {
+      navigate("/cart");
+    } else {
+      navigate("/login");
+    }
+  };
+
+  const addToFavHandler = () => {
+    if (isAuthenticated) {
+      navigate("/wishlist");
+    } else {
+      navigate("/login");
+    }
+  };
+
   return (
     <div className={classes.card}>
       <h2 className={classes.productName}>{name}</h2>
@@ -8,8 +32,8 @@ const ProductItem = ({ name, description, price, category, isFavourite }) => {
       <p className={classes.productPrice}>{price}</p>
       <p className={classes.productCategory}>{category}</p>
       <div className={classes.options}>
-        <button>Add to Cart</button>
-        <button>Add to Favourite</button>
+        <button onClick={addToCartHandler}>Add to Cart</button>
+        <button onClick={addToFavHandler}>Add to Favourite</button>
       </div>
     </div>
   );
