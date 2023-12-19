34e04e3942baac85fdee9229208afdad0685796b
Shanya <shanyaverma03@gmail.com>
2023-12-20 02:12:09 +0530

    Show Cart

--- src/components/products/Cart.js
+++ src/components/products/Cart.js
@@ -1,3 +1,24 @@
-const Cart = () => {};
+import { useSelector } from "react-redux";
+
+import CartItem from "./CartItem";
+
+const Cart = () => {
+  const cart = useSelector((state) => state.cart.cart);
+
+  return (
+    <div>
+      {cart &&
+        cart.map((item) => (
+          <CartItem
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            price={item.price}
+            quantity={item.quantity}
+          />
+        ))}
+    </div>
+  );
+};
 
 export default Cart;
--- src/components/products/CartItem.js
+++ src/components/products/CartItem.js
@@ -1,14 +1,14 @@
 import classes from "./CartItem.module.css";
 
-const CartItem = () => {
+const CartItem = ({ id, name, price, quantity }) => {
   return (
     <div className={classes.wrapper}>
       <div className={classes.card}>
-        <h3 className={classes.name}>Name</h3>
-        <p className={classes.price}>$10 per item</p>
+        <h3 className={classes.name}>{name}</h3>
+        <p className={classes.price}>${price} per item</p>
         <div className={classes.quantity_control}>
           <button>-</button>
-          <span className={classes.quantity}>2</span>
+          <span className={classes.quantity}>{quantity}</span>
           <button>+</button>
         </div>
         <p className={classes.total_price}>Total:88</p>
